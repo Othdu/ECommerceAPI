45154e7dfbab8df8900d346b7b5a024f2a358a85
Othdu <moh.mus2019@gmail.com>
2026-06-11 20:34:00 +0300

    Add pagination and filtering to products endpoint

---
+++ ECommerceAPI/DTOs/PagintionDto.cs
@@ -0,0 +1,35 @@
+﻿namespace ECommerceAPI.DTOs
+{
+    public class PaginationParams
+    {
+        private const int MaxPageSize = 50;
+        private int _pageSize = 10;
+
+        public int Page { get; set; } = 1;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+
+
+        }
+
+        public string? Search { get; set; }
+        public String? SortBy { get; set; }
+        public bool SortDescending { get; set; } = false;
+
+
+
+    }
+    public class PagedResult<T>
+    {
+        public List<T> Data { get; set; } = new();
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+        public bool HasNextPage => Page < TotalPages;
+        public bool HasPreviousPage => Page > 1;
+    }
+}
